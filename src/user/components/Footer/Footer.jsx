b9a4bcdba7495f4128f360ede366ff834991ba84
gaurav <gk914670@gmail.com>
2024-12-13 11:29:56 +0530

    fixed footer

--- src/user/components/Footer/Footer.jsx
+++ src/user/components/Footer/Footer.jsx
@@ -8,12 +8,6 @@ function Footer() {
     <>
       <footer className="bg-black text-white">
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col ">
-          <div className="w-64 md:mx-0 mx-auto text-center md:text-left">
-            <h3 className="font-bol text-2xl">
-              Puja<span className="text-red-500 font-bold text-2xl">Vivah</span>
-            </h3>
-          </div>
-
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium tracking-widest text-lg mb-3">
@@ -63,12 +57,13 @@ function Footer() {
             </div>
           </div>
         </div>
+
         <div className="bg-red-600 text-white">
-          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-sm text-center sm:text-left">
-              © 2024 PujaVivah -<span>@copyright</span>
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col items-center">
+            <p className="text-sm text-center">
+              © 2024 - All rights reserved by <a href="https://mithilastack.com/" >MithilaStack</a>
             </p>
-            <p className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            {/* <p className="inline-flex mt-4 justify-center">
               <span className="mr-2">
                 <FaFacebook size={30} />
               </span>
@@ -78,7 +73,7 @@ function Footer() {
               <span className="mr-2">
                 <FaTwitter size={30} />
               </span>
-            </p>
+            </p> */}
           </div>
         </div>
       </footer>
